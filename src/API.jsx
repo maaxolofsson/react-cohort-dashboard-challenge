e8e5ea1e8026f197d754bcfe8e1183d1c23c805d
Max V Olofsson <max.olofsson@se.experis.com>
2024-10-03 09:11:55 +0200

    Added base files and first draft of component tree

---
+++ src/API.jsx
@@ -0,0 +1,45 @@
+// This file is taken from my implementation from the exercise "react-address-book-part-2"
+const baseURL = "https://boolean-uk-api-server.fly.dev/"
+const githubUsername = "maaxolofsson/"
+
+export async function get(endpoint) {
+    const res = await fetch(baseURL + githubUsername + endpoint);
+    const data = await res.json();
+    return data
+}
+
+export async function deleteOne(endpoint) { // Endpoint should include ID, e.g. /contact/10
+    const c = {
+        method: 'DELETE'
+    }
+    console.log(baseURL + githubUsername + endpoint)
+    const httpRes = await fetch(baseURL + githubUsername + endpoint, c);
+    const data = await httpRes.json();
+    return { httpRes, data }
+}
+
+export async function post(endpoint, data) { // data = obj (json)
+    const c = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    }
+    const httpRes = await fetch(baseURL + githubUsername + endpoint, c)
+    const resData = await httpRes.json()
+    return { httpRes, resData };
+}
+
+export async function put(endpoint, newData) { // data = obj (json)
+    const c = {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(newData)
+    }
+    const httpRes = await fetch(baseURL + githubUsername + endpointd, c);
+    const resData = await httpRes.json()
+    return { httpRes, resData };
+}
